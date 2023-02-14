34250b93a53b2eca770fd89a8b076941b4de2e8c
Clavier Eliott <eliott.clavier@epitech.eu>
2023-02-14 16:07:12 +0100

    Add download of the image from Internet

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from src.window import Window
 
 def main():
-    Window()
-
-    # try:
-    #     BrailleConverter()
-    #     ascii_converter = AsciiConverter()
-    #     if len(sys.argv) > 1:
-    #         ascii_converter.launch_workflow(sys.argv[1], "ascii_image")
-    #     else:
-    #         raise Exception("No image path provided")
-    # except Exception as e:
-    #     print(e)
+    window = Window()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
--- src/window.py
+++ src/window.py
@@ -1,51 +1,68 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
+
+import shutil
+import requests
 
 from src.ascii_converter import AsciiConverter
 
-import sys
 from os import path
 
-class Window:
+import sv_ttk
 
-    window: Tk = None
+class Window(tk.Tk):
+
     ascii_converter: AsciiConverter = AsciiConverter()
-
-    previous_ascii_image: StringVar = None
+    previous_ascii_image: tk.StringVar = None
+    url_entry: ttk.Entry = None
 
     def __init__(self):
-        try:
-            if len(sys.argv) > 1:
-                self.write_base_window()
-            else:
-                raise FileNotFoundError("No image path provided")
-        except Exception as e:
-            self.exception_catcher(e)
+        super().__init__()
+        self.write_base_window()
 
     def exception_catcher(self, e) -> None:
-        messagebox.showerror("Error", e)
+        tk.messagebox.showerror("Error", e)
 
-    def write_base_window(self) -> None:
-        self.window = Tk()
-        self.window.title("Shriik's Image Converter")
+    def write_base_window(self) -> None:        
+        self.title("Shriik's Image Converter")
 
-        self.previous_ascii_image = StringVar()
-                
-        ascii_button = Button(self.window, text="Convert to ASCII", command=self.convert_ascii)
-        ascii_button.grid(column=1, row=1, padx=(100, 100), pady=(100, 100))
+        self.previous_ascii_image = tk.StringVar()
 
-        previous_ascii_image = Label(self.window, textvariable=self.previous_ascii_image, font=("Courier", 5))
-        previous_ascii_image.grid(column=2, row=1, padx=(10, 100), pady=(100, 100))
+        self.url_entry = ttk.Entry(self, width=50)
+        self.url_entry.insert(0, 'Enter an image URL')
+        self.url_entry.grid(column=0, row=0, padx=(50, 50), pady=(100, 10))
+        self.url_entry.bind("<Return>", lambda e: self.convert_ascii())
+     
+        previous_ascii_image = ttk.Label(self, textvariable=self.previous_ascii_image, font=("Courier", 5))
+        previous_ascii_image.grid(column=0, row=1, padx=(50, 50), pady=(100, 100))
 
         if path.exists("./ascii/ascii_image.txt"):
             with open("./ascii/ascii_image.txt", "r") as f:
                 self.previous_ascii_image.set(f.read())
                 f.close()
 
-        self.window.mainloop()
+        sv_ttk.set_theme("dark")
     
     def convert_ascii(self) -> None:
-        self.ascii_converter.launch_workflow(sys.argv[1], "ascii_image")
-        with open("./ascii/ascii_image.txt", "r") as f:
-            self.previous_ascii_image.set(f.read())
-            f.close()
+        url = self.url_entry.get()
+        img_format = url.split('.')[-1]
+
+        format_accepted = ("jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp", "ico", "svg", "raw")
+        if img_format in format_accepted:
+            image_name = url.split('/')[-1]
+            res = requests.get(url, stream = True)
+            
+            if res.status_code == 200:
+                with open(f"./images/{image_name}", 'wb') as f:
+                    shutil.copyfileobj(res.raw, f)
+                print(f"Image sucessfully downloaded: {image_name}")
+
+                self.ascii_converter.launch_workflow(f"./images/{image_name}", "ascii_image")
+                with open("./ascii/ascii_image.txt", "r") as f:
+                    self.previous_ascii_image.set(f.read())
+                    f.close()
+            else:
+                self.exception_catcher("Image couldn't be retrieved")
+        else:
+            self.exception_catcher(f"Format {img_format} not accepted")
